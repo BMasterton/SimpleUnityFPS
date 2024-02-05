8c9a5dca63ac6274c730b9acd445d9ec6f2c749d
Braonain Masterton <99563726+BMasterton@users.noreply.github.com>
2024-02-05 15:19:27 -0800

    added multiple enemies

--- Assets/Script/SceneController.cs
+++ Assets/Script/SceneController.cs
@@ -6,31 +6,31 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField]
     private GameObject enemyPrefab;
-    private GameObject enemy;
+    //private GameObject enemy;
     private Vector3 spawnPoint = new Vector3(0, 0, 5);
     private int enemyAmount = 5;
-    private GameObject[] enemies;
+    private GameObject[] enemies = new GameObject[5];
     // Update is called once per frame
 
 
-    private void Start()
-    {
-        for (int i = 0; i < enemyAmount; i++) { 
-            enemies[i] = enemy ;
+   //private void Start()
+    //{
+    //    for (int i = 0; i < enemyAmount; i++) { 
+    //        enemies[i] = enemy ;
         
-        }
+    //    }
         
-    }
+    //}
     void Update()
     {
         for (int i = 0; i < enemyAmount; i++)
         {
-            if (enemy == null)
+            if (enemies[i] == null)
             {
-                enemy = Instantiate(enemyPrefab) as GameObject;
-                enemy.transform.position = spawnPoint;
+                enemies[i] = Instantiate(enemyPrefab) as GameObject;
+                enemies[i].transform.position = spawnPoint;
                 float angle = Random.Range(0, 360);
-                enemy.transform.Rotate(0, angle, 0);
+                enemies[i].transform.Rotate(0, angle, 0);
             }
         }
     }
